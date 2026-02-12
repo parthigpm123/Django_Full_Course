8167415802897ddd9ac9a10bfb3510cf22b6f8da
parthigpm123 <parthigpm123@.com>
2026-02-12 21:28:42 +0530

    a

--- crudDemo/crudApp/views.py
+++ crudDemo/crudApp/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from crudApp.models import Student
+from crudApp.forms import StudentForm
 
 # Create your views here.
 
 def retrieve_view(request):
       student= Student.objects.all()
       return render(request, 'crudApp/index.html', {'student': student})
+
+def create_view(request):
+      form = StudentForm()
+      return render(request, 'crudApp/create.html', {'form': form})
